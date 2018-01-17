b367022c1b8b0400704881b1e4db771c69096a27
drdlee <danielrudi.lee@gmail.com>
2018-01-17 22:40:49 +0800

    bikin query scope, dimana ini maksudnya kita udah bikin function untuk cari data2 tertentu dan juga bisa di tambah kondisi lainnya.

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+  public function scopeIncomplete($query){  // cara panggilnya, Task::incomplete()->get();
+    return $query->where('completed', 0);   // cara panggil pakai kondisi, Task::incomplete()->where('id', '>=', 2)->get();
+  }
 }
