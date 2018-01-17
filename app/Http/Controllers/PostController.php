df704aaf48d9e63bbc06f1b3a0bd8975fb889383
drdlee <danielrudi.lee@gmail.com>
2018-01-18 00:51:44 +0800

    cara mass assignment POST request

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,10 +14,16 @@ class PostController extends Controller
       return view('posts.create');
     }
     public function store(){
-      $post = new Post;
-      $post->title = request('title');
-      $post->body = request('body');
-      $post->save();
+      // $post = new Post;
+      // $post->title = request('title');
+      // $post->body = request('body');
+      // $post->save();
+
+      Post::create([
+        'title' => request('title'),
+        'body' => request('body')
+      ]);   //   ini namanya mass assignment dimana laravel akan butuh sentingan di Model Post, kalau mau pakai cara ini
+
       return redirect('/posts');
     }
 }
