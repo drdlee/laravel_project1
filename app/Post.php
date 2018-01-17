4fbaec13d83bd8c7bde14fd5e992fd69e28ab78c
drdlee <danielrudi.lee@gmail.com>
2018-01-18 00:54:06 +0800

    POST request, settingan lain di Model. ini kebalikan dari $fillable,

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'body'];
-}
+    // protected $fillable = ['title', 'body'];
+    protected $guarded = []; //  artinya field yang terjaga.. disini arraynya kosong,
+}                            //  artinya ngga ada field yang terjaga
